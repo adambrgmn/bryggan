7c36b6c5b2211311c85e9aa76df857f95762aeb2
Adam Bergman <adam@fransvilhelm.com>
2023-02-27 09:02:49 +0100

    Try fixing fb private key issue

--- lib/clients/firebase-admin.ts
+++ lib/clients/firebase-admin.ts
@@ -5,7 +5,7 @@ try {
     databaseURL: 'https://sst-bryggan.firebaseio.com',
     credential: firebase.credential.cert({
       projectId: 'sst-bryggan',
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: JSON.parse(process.env.FIREBASE_PRIVATE_KEY ?? ''),
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
   });
