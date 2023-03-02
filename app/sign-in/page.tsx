3d35c9c552914274732a83d555e54d3d10fbaf62
Adam Bergman <adam@fransvilhelm.com>
2023-03-02 21:55:51 +0100

    Remove unused

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth';
-import { redirect, useSearchParams } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 import { Auth } from '@/components/Auth';
 import { config } from '@/lib/config';
